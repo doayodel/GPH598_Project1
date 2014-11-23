c91a9d797a35a4ea902ae22e42a949ac4672e200
bspark05 <bpark21@asu.edu>
2014-11-22 23:32:47 -0700

    Update Polygon.py

--- Polygon.py
+++ Polygon.py
@@ -13,3 +13,9 @@ def makeConvexHull(points, incremental=False, qhull_options=None):
     for j in hull.vertices:
         convexHullArray.append([points[j,0], points[j,1]])
     return convexHullArray
+    
+ def makeMBR(points):
+    min_x, min_y = np.min(points, axis=0)
+    max_x, max_y = np.max(points, axis=0)
+    mbrArray = np.array([(min_x, min_y), (max_x, min_y), (max_x, max_y), (min_x, min_y)])
+    return mbrArray
