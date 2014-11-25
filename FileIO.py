8c2d97070f758841e56d829fdafcf21ead6d5d20
doayodel <doayodel@users.noreply.github.com>
2014-11-25 13:19:36 -0700

    Update FileIO.py

--- FileIO.py
+++ FileIO.py
@@ -4,7 +4,10 @@ Created on Nov 20, 2014
 @author: Bumsub
 '''
 
-import pysal
+The function 'openFile' can open csv, txt, dbf and shp files
+
+# 'seek' finds the first row and 'append' adds subsequent rows
+# 'outputArray' returns a list data type
 
 def openFile(connectionString, mode='r'):
         point = pysal.pysal.core.FileIO.FileIO.open(connectionString, mode='r')  # @UndefinedVariable
