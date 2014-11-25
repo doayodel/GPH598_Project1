ff7333e98750113f0bff0836b42e374701f3da1f
bspark05 <bpark21@asu.edu>
2014-11-25 12:16:16 -0700

    Update FileIO.py

--- FileIO.py
+++ FileIO.py
@@ -9,17 +9,6 @@ import pysal
 def openFile(connectionString, mode='r'):
     shp = pysal.pysal.core.FileIO.FileIO.open(connectionString, mode='r')  # @UndefinedVariable
     shp.seek(0)
-    
-@author: Kailai 
-
-import pysal
-shp = pysal.open('E:/PUP598 geocomputation/Cities.shp')
-print len(shp)
-
-import pysal
-dbf = pysal.open('E:/PUP598 geocomputation/Cities.dbf')
-print len(dbf)
-
-import pysal
-csv = pysal.open('E:/PUP598 geocomputation/Cities.csv')
-print len (csv)
+    outputArray = []
+    for i in shp:
+        outputArray.append(i)
